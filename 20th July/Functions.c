335e01d70348734c428cc0b142a5ef8a60070c14
tirth272 <shahtirth24680@gmail.com>
2022-07-20 17:25:10 +0530

    20th july

---
+++ 20th July/Functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void func1()
+{
+    printf("Function 1 called!!!\n");
+}
+void func2()
+{
+    printf("Function 2 called!!!\n");
+}
+void func3()
+{
+    printf("Function 3 called!!!\n");
+}
+int main()
+{
+    func1();
+    func2();
+    func3();
+    return 0;
+}
---
+++ 20th July/function2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void add()
+{
+}
+void sub()
+{
+}
+void mul()
+{
+}
+void div()
+{
+}
+
+int main()
+{
+
+    return 0;
+}
