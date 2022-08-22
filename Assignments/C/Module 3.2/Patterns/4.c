cdcf8e7f697b656043481986b46c74758c0beedf
tirth272 <shahtirth24680@gmail.com>
2022-08-22 15:35:55 +0530

    ok

---
+++ Assignments/C/Module 3.2/Patterns/4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int rows, i, j, n = 1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; ++j)
+        {
+            printf("%d ", n);
+            ++n;
+        }
+        printf("\n");
+    }
+    return 0;
+}
